74bd868fd65a24029e824818c97f53ae83c482c9
binz <binz123456>
2020-09-16 20:19:08 +0800

    init1

--- server/server.go
+++ server/server.go
@@ -1,22 +1,9 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"fmt"
 	"net/http"
 )
-
-func handler() {
-	fmt.Fprint()
-}
-
-func main() {
-	http.ListenAndServe("8080", handler)
-=======
-	//api_get "api.get"
-	"net/http"
-)
-import "fmt"
 
 func getLaravel(w http.ResponseWriter, req *http.Request) {
 	//html := api_get.Get("http://192.168.10.113:8011" + req.URL.String())
@@ -34,6 +21,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
->>>>>>> 3ac2f7ee3151bc5cc4a1a5bac49e644b435dd9b3
 }
