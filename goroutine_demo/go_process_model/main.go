5f5340d729076a5218365954b271c48694f7d592
binz <876507628@qq.com>
2020-11-12 23:10:10 +0800

    test go process

--- goroutine_demo/go_process_model/main.go
+++ goroutine_demo/go_process_model/main.go
@@ -8,6 +8,42 @@ import (
 )
 
 func main() {
+
+	test2()
+}
+
+func test2() {
+	max := 16
+	sLen := 12
+
+	sliceInt := make([]int, sLen)
+
+	runtime.GOMAXPROCS(sLen)
+
+	wait := sync.WaitGroup{}
+	wait.Add(max)
+	ch := make(chan int)
+
+	for index := 0; index < max; index++ {
+		go func(goInt []int, index int) {
+			res := <-ch
+			time.Sleep(time.Millisecond * 2)
+			wait.Done()
+			fmt.Println(index, "index", res)
+		}(sliceInt, index)
+	}
+
+	// 发送信号
+	for index := 0; index < max; index++ {
+		ch <- index
+	}
+
+	wait.Wait()
+	fmt.Println("Finish")
+	fmt.Println(sliceInt)
+}
+
+func test1() {
 	max := 16
 	sLen := 12
 
@@ -23,6 +59,7 @@ func main() {
 			for i := 0; i < len(sliceInt); i++ {
 				if sliceInt[i] == 0 {
 					//mu.Lock()
+
 					sliceInt[i]++
 					time.Sleep(time.Millisecond)
 					//mu.Unlock()
