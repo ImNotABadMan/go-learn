be38ddd765e682b5869d520afeee8e1d66946f24
binz <876507628@qq.com>
2020-11-15 22:55:14 +0800

    go test

--- goroutine_demo/go_process_model/main.go
+++ goroutine_demo/go_process_model/main.go
@@ -3,13 +3,60 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sort"
+	"strconv"
 	"sync"
 	"time"
 )
 
 func main() {
+	//test1()
+	//test2()
+	test3()
+}
 
-	test2()
+func test3()  {
+	max := 16
+	sLen := 12
+
+	sliceInt := make([]string, 0)
+	runtime.GOMAXPROCS(sLen)
+	wait := sync.WaitGroup{}
+	wait.Add(max)
+
+	for index := 0; index < max; index++ {
+		start := time.Now().UnixNano()
+		//sliceInt = append(sliceInt, strconv.FormatInt(int64(index), 10) +
+		//	" go start " + strconv.FormatInt(start, 10))
+		go func(goInt []string, index int) {
+			//start := time.Now().UnixNano()
+			//sliceInt = append(sliceInt, strconv.FormatInt(int64(index), 10) +
+			//	" go start " + strconv.FormatInt(start, 10))
+			//fmt.Println(sliceInt)
+			//h,m,s := time.Now().Clock()
+			//fmt.Println(index, "go start" , h , ":", m, ":", s)
+			time.Sleep(time.Millisecond)
+			sliceInt = append(sliceInt, strconv.FormatInt(int64(index), 10) +
+				" go end " + strconv.FormatInt(time.Now().UnixNano(), 10) + " 差：" +
+				strconv.FormatInt(time.Now().UnixNano() - start, 10))
+
+
+			//h,m,s = time.Now().Clock()
+			//fmt.Println(index, "go end", time.Now().Unix(), h , ":", m, ":", s)
+
+			wait.Done()
+		}(sliceInt, index)
+	}
+
+	wait.Wait()
+	fmt.Println("Finish")
+	//fmt.Println(sliceInt)
+	sort.Slice(sliceInt, func(i, j int) bool {
+		return sliceInt[i] > sliceInt[j]
+	})
+	for _, item := range sliceInt{
+		fmt.Println(item)
+	}
 }
 
 func test2() {
